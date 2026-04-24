6ab12a1cdd74813e8912431d660c0f568aa7f0dc
sununta-sk <sk.wtf2803@gmail.com>
2026-04-24 19:12:16 +0700

    fix: import index.css in main.jsx (was missing entirely)

--- Dating-App/src/main.jsx
+++ Dating-App/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import './index.css'
 import ErrorBoundary from './components/admin/ErrorBoundary'
 import { assertNoServiceRoleKey, injectCSP } from './utils/security'
 
